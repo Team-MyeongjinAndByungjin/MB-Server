7ebc1b3c25d5686e3aef1aea2d4abd96ec5e3eac
Kangmin <ehfehf0629@gmail.com>
2023-07-05 01:55:47 +0900

    feat: coupon logic

--- MB-Server/src/main/java/team/mb/mbserver/domain/coupon/entity/Coupon.java
+++ MB-Server/src/main/java/team/mb/mbserver/domain/coupon/entity/Coupon.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import team.mb.mbserver.domain.user.entity.User;
+import team.mb.mbserver.global.error.BusinessException;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -26,16 +27,26 @@ public class Coupon {
     @Column(nullable = false)
     private LocalDate expiredAt;
 
+    @Column(nullable = false)
+    private int price;
+
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Builder
-    public Coupon(Long id, String name, String imageUrl, LocalDate expiredAt, User user) {
-        this.id = id;
+    public Coupon(String name, String imageUrl, int price, LocalDate expiredAt, User user) {
         this.name = name;
+        this.price = price;
         this.imageUrl = imageUrl;
         this.expiredAt = expiredAt;
+        this.user = user;
+    }
+
+    public void giveCoupon(User currentUser, User user) {
+        if (this.user != currentUser) {
+            throw new BusinessException(401, "쿠폰 주인이 아닙니다.");
+        }
         this.user = user;
     }
 }
