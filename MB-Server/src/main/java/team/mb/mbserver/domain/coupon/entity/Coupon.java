3fc38ec8007aeffd2d0d395d9cd0a7d48abab71d
4mjeo <4mj.eo@dsm.hs.kr>
2023-07-04 21:11:20 +0900

    chore: Coupon

--- MB-Server/src/main/java/team/mb/mbserver/domain/coupon/entity/Coupon.java
+++ MB-Server/src/main/java/team/mb/mbserver/domain/coupon/entity/Coupon.java
@@ -1,5 +1,7 @@
 package team.mb.mbserver.domain.coupon.entity;
 
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import team.mb.mbserver.domain.user.entity.User;
 
@@ -7,29 +9,34 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+
+@Getter
 @Entity
 @NoArgsConstructor
 @Table(name = "coupon")
 public class Coupon {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
-
-    @Column(nullable = false)
-    private String name;
-
-    @Column(nullable = false)
-    private String image;
 
     @Column(nullable = false)
     private LocalDate expiredAt;
 
     @Column(nullable = false)
-    private Integer price;
+    private String name;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @ManyToOne
     @JoinColumn(name = "user_id",nullable = false)
     private User user;
 
+    @Builder
+    public Coupon(Long id, String name,String imageUrl) {
+        this.id = id;
+        this.name= name;
+        this.imageUrl = imageUrl;
+    }
 }
