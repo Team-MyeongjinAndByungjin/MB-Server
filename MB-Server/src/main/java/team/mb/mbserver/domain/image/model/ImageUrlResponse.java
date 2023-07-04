5c0fe50c25953566582867280446b6edec665ab6
Kangmin <ehfehf0629@gmail.com>
2023-07-04 21:14:37 +0900

    feat: ImageUrlResponse

---
+++ MB-Server/src/main/java/team/mb/mbserver/domain/image/model/ImageUrlResponse.java
@@ -0,0 +1,11 @@
+package team.mb.mbserver.domain.image.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ImageUrlResponse {
+
+    private final String imageUrl;
+}
