bec52345daa3dd5c639571fae5613e088b6cee33
Kangmin <ehfehf0629@gmail.com>
2023-07-04 20:24:40 +0900

    feat: S3 feature

---
+++ MB-Server/src/main/java/team/mb/mbserver/global/storage/S3Facade.java
@@ -0,0 +1,57 @@
+package team.mb.mbserver.global.storage;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import team.mb.mbserver.global.error.BusinessException;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class S3Facade {
+
+    private final AmazonS3Client amazonS3Client;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String uploadImage(MultipartFile image) {
+        if (image.isEmpty()) {
+            throw new BusinessException(404, "Image Not Found");
+        }
+
+        String fileName = bucket + UUID.randomUUID() + image.getOriginalFilename();
+
+        try {
+            PutObjectRequest putObjectRequest = new PutObjectRequest(
+                    bucket,
+                    fileName,
+                    image.getInputStream(),
+                    getObjectMetadata(image)
+            );
+
+            amazonS3Client.putObject(putObjectRequest.withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (Exception e) {
+            throw new BusinessException(400, "Save Image Failed");
+        }
+
+        return getFileUrl(fileName);
+    }
+
+    private ObjectMetadata getObjectMetadata(MultipartFile image) {
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(image.getSize());
+        objectMetadata.setContentType(image.getContentType());
+
+        return objectMetadata;
+    }
+
+    private String getFileUrl(String fileName) {
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+}
