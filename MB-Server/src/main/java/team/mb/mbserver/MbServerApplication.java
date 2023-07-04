5d8780a5a2b2f16d807b12653eda5e803e6d3298
Kangmin <ehfehf0629@gmail.com>
2023-07-04 13:11:00 +0900

    setting: project

---
+++ MB-Server/src/main/java/team/mb/mbserver/MbServerApplication.java
@@ -0,0 +1,13 @@
+package team.mb.mbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbServerApplication.class, args);
+    }
+
+}
---
+++ MB-Server/src/test/java/team/mb/mbserver/MbServerApplicationTests.java
@@ -0,0 +1,13 @@
+package team.mb.mbserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
