d56d7cf93e8f029eb6c3708dba4cab07704a0f77
Kangmin <ehfehf0629@gmail.com>
2023-07-04 16:28:48 +0900

    chore: SecurityConfig

--- MB-Server/src/main/java/team/mb/mbserver/global/security/SecurityConfig.java
+++ MB-Server/src/main/java/team/mb/mbserver/global/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package team.mb.mbserver.global.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,10 +9,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import team.mb.mbserver.global.jwt.JwtProvider;
 
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
 
+    private final JwtProvider jwtTokenProvider;
+    private final ObjectMapper objectMapper;
+
+    @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
@@ -23,12 +31,14 @@ public class SecurityConfig {
 
         http
                 .authorizeRequests()
-                .antMatchers("/user/**").permitAll()
-                .antMatchers(("/images/**")).permitAll()
+                .antMatchers("/users", "/users/token").permitAll()
                 .antMatchers("/swagger-ui/**").permitAll()
                 .antMatchers("/v3/api-docs/**").permitAll()
                 .antMatchers("/api-docs").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+
+        .and()
+                .apply(new FilterConfig(jwtTokenProvider, objectMapper));
 
         return http.build();
     }
