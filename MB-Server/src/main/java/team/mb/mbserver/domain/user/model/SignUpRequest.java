8ad0f75f4099b1904f222aa3008e9a7c42a3e719
Kangmin <ehfehf0629@gmail.com>
2023-07-04 15:18:25 +0900

    feat: SignUpRequest

---
+++ MB-Server/src/main/java/team/mb/mbserver/domain/user/model/SignUpRequest.java
@@ -0,0 +1,17 @@
+package team.mb.mbserver.domain.user.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+public class SignUpRequest {
+
+    @NotBlank
+    private String accountId;
+    
+    @NotBlank
+    private String password;
+}
